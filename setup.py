8958553c84bd826d111407bed7bc4555d5e7278e
Prakash Chaudhary <01prakashchaudharyc@gmail.com>
2026-07-23 07:54:40 +0545

    logging and exception

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ HYPEN_E_DOT='-e .'
 
 def get_requirements(file_path: str)->List[str]:
     """
-    this function will return the list of requiremnets
+    this function will return the list of requirements
     """
     requirements=[],
     with open(file_path) as file_obj:
         requirements=file_obj.readlines()
         requirements=[req.replace("\n","") for req in requirements]
 
-        if HYPEN_E_DOT in requirements: 
+        if HYPEN_E_DOT in requirements:
             requirements.remove(HYPEN_E_DOT)
 
     return requirements
