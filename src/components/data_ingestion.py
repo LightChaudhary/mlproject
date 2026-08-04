be67316d75ec7e0b6f5fa7aa941a54123e3083bc
Prakash Chaudhary <01prakashchaudharyc@gmail.com>
2026-08-04 22:12:19 +0545

    docs: add inline comments to ETL pipeline main block

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -71,5 +71,6 @@ if __name__ == "__main__":
     obj = DataIngestion()
     train_data,test_data = obj.initiate_data_ingestion()
 
+    # Initialize transformer and apply preprocessing to ingested train/test splits.
     data_transformation = DataTransformation()
     data_transformation.initiate_data_transformation(train_data,test_data)
