f4674d0c7dbb707988005239184af5ae4c281ced
Prakash Chaudhary <01prakashchaudharyc@gmail.com>
2026-08-07 23:43:28 +0545

    feat: add model evaluation utility

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,8 @@ import dill
 import numpy as np
 import pandas as pd
 
+from sklearn.metrics import r2_score
+
 from src.exception import CustomException
 
 def save_object(file_path, obj):
@@ -16,5 +18,28 @@ def save_object(file_path, obj):
         with open(file_path, "wb") as file_obj:
             dill.dump(obj, file_obj)
 
+    except Exception as e:
+        raise CustomException(e, sys)
+
+def evaluate_models(X_train, y_train, X_test, y_test, models):
+    """
+    Evaluate multiple regression models and return their r2 scores.
+    """
+    try:
+        report = {}
+
+        for i in range (len(list(models))):
+            model = list(models.values())[i]
+            model.fit(X_train, y_train) # Train model
+
+            y_train_pred = model.predict(X_train) # Predict on training data
+            y_test_pred = model.predict(X_test) # Predict on test data  
+
+            train_model_score = r2_score(y_train, y_train_pred) # Calculate r2 score for training data
+            test_model_score = r2_score(y_test, y_test_pred) # Calculate r2 score for test data
+
+            report[list(models.keys())[i]] = test_model_score # Store test r2 score in report
+
+        return report
     except Exception as e:
         raise CustomException(e, sys)
